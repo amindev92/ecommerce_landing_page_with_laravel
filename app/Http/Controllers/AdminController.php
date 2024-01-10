b9e7ffe2a540c3f09606c87a523cbbcc62297db2
Amin Ataei <amin.ataei92@gmail.com>
2024-01-10 13:14:51 +0330

    update product operate in admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,25 +10,55 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
 
-    public function delete_product($id){
+    public function update_product_item(Request $request, $id)
+    {
+        $product = product::find($id);
+
+        $product->product_title = $request->product_title;
+        $product->product_price = $request->product_price;
+        $product->product_description = $request->product_description;
+        $product->product_category = $request->product_category;
+        $product->product_discount = $request->discount_price;
+        $product->product_quantity = $request->product_quantity;
+
+
+        $image = $request->product_image;
+        if ($image) {
+            $imageName = time() . "." . $image->getClientOriginalExtension();
+            $request->product_image->move('product_img', $imageName);
+            $product->product_image = $imageName;
+        }
+        $product->save();
+
+        return redirect()->back()->with('message', 'update product succesfully');
+    }
+
+    public function update_product($id)
+    {
+        $product = product::find($id);
+        $category = category::all();
+        return view("admin.update_product", compact("product", "category"));
+    }
+
+    public function delete_product($id)
+    {
 
         $product = product::find($id);
 
         $product->delete();
 
-        return redirect()->back()->with("message","delete product item successfully");
-
-
+        return redirect()->back()->with("message", "delete product item successfully");
     }
 
-    public function show_product(){
+    public function show_product()
+    {
 
         $products = product::all();
-        return view("admin.show_product",compact("products"));
-        
+        return view("admin.show_product", compact("products"));
     }
 
-    public function add_product_item(Request $request){
+    public function add_product_item(Request $request)
+    {
 
         $product = new product;
 
@@ -41,51 +71,49 @@ class AdminController extends Controller
 
 
         $image = $request->product_image;
-        $imageName = time().".".$image->getClientOriginalExtension();
+        $imageName = time() . "." . $image->getClientOriginalExtension();
         $request->product_image->move('product_img', $imageName);
         $product->product_image = $imageName;
 
         $product->save();
 
-        return redirect()->back()->with('message','add product succesfully');
-
+        return redirect()->back()->with('message', 'add product succesfully');
     }
 
-    public function view_product(){
+    public function view_product()
+    {
 
         $category = category::all();
 
         return view("admin.product", compact("category"));
-
     }
-    
-    public function delete_category($id){
+
+    public function delete_category($id)
+    {
         $category = category::find($id);
 
         $category->delete();
 
-        return redirect()->back()->with("message","delete category item successfully");
-
+        return redirect()->back()->with("message", "delete category item successfully");
     }
 
-    public function view_category(){
+    public function view_category()
+    {
 
         $data = category::all();
 
         return view("admin.category", compact("data"));
-
     }
 
-    public function add_category(Request $request){
+    public function add_category(Request $request)
+    {
 
         $data = new category;
-        
+
         $data->category_name = $request->categoryName;
 
         $data->save();
-        
-        return redirect()->back()->with('message',"Successfully in add category");
 
+        return redirect()->back()->with('message', "Successfully in add category");
     }
-
 }
